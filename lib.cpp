a5d18f70483c96d7d1077465593471eb20c5b220
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-12-12 11:38:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+float funzione (float b[], int h){
+    float max= b[0];
+    for (int i=0;i < h;i++) {
+        if (max < b[i]) {
+            max = b[i];
+        }
+    }
+    return max;
+}
