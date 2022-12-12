283205c155da5ee5675045663f8a67cca87cccc8
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-12-12 11:34:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    int x;
+    float y;
+    float a[x];
+    cout << "quale è la dimensione dell'array?" << endl;
+    cin >> x;
+    for (int i = 0; i < x; i++) {
+        cout << "quale è il valore che vuoi inserire?" << endl;
+        cin >> y;
+        a[i] = y;
+    }
+    cout<< funzione(a, x);
 }
