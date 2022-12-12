42f282dfa54d676de366c3f2bef2ed9a327a9af1
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-12-12 11:42:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
 #include "lib.h"
-using namespace std;
-
-float funzione (float b[], int h){
-    float max= b[0];
-    for (int i=0;i < h;i++) {
-        if (max < b[i]) {
-            max = b[i];
+float funzione(float numeri[], int dimensione){
+    float massimo= numeri[0];
+    for (int i=0;i < dimensione;i++) {
+        if (massimo < numeri[i]) {
+            massimo = numeri[i];
         }
     }
-    return max;
+    return massimo;
 }
